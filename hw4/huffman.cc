796b7ac9c213835d2a74646a80518cf2a519cc9f
ReynardV <31481461+ReynardV@users.noreply.github.com>
2018-02-21 11:47:06 -0800

    Add files via upload

--- hw4/huffman.cc
+++ hw4/huffman.cc
@@ -1,20 +1,24 @@
 #include "huffman.hh"
+#include <unordered_map>
+#include <queue>
 
 namespace huffman {
-	uint8_t EOF = 256 //lol idk what EOF I can safely use yet
+	using symbol_t = Huffman::symbol_t;
+	using encoding_t = Huffman::encoding_t;
+	int end = 255+1; 
 
 	struct Huffman::Impl {
 		Impl();
-		addSymbol(symbol_t symbol);
+		void addSymbol(symbol_t symbol);
 		struct compareValues;
-		makeTree();
+		void makeTree();
 		std::unordered_map<symbol_t, int> freqMap; //frequency map is to match the symbol to its frequency
 		std::unordered_map<symbol_t, encoding_t> encMap; //encoding map matches the symbol to its binary encoding
+		bool edited = true;
 	};
 
-
 	Huffman::Impl::Impl() 
-		:freqMap({{EOF,1}}), encMap({{EOF,{}}})
+		:freqMap({{end,1}}), encMap({{end,{}}})
 	{
 		for (unsigned i = 0; i < 256; ++i) {
 			freqMap.insert({i, 0}); 
@@ -22,6 +26,7 @@ namespace huffman {
 		};
 	}
 
+	void
 	Huffman::Impl::addSymbol(symbol_t symbol) {
 		freqMap[symbol]++;
 	}
@@ -33,14 +38,14 @@ namespace huffman {
 	};
 
 	//I'm not really sure I can explain what happened here. I had a clever idea and then it got... out of hand.
-
+	void
 	Huffman::Impl::makeTree(){
 		std::priority_queue<std::pair<std::vector<symbol_t>, int>, std::vector<std::pair<std::vector<symbol_t>, int>>, Huffman::Impl::compareValues> symbolSort;
 		// Oh boy. symbolSort is a priority queue that accepts pairs, and sorts them so the pair with the smallest value is on top. 
 		// The pairs it accepts contain one vector of symbols, and an int. 
 
 		std::unordered_map<symbol_t, int>::iterator it;
-		for (it = freqMap.begin(); it = freqMap.end(); it++){
+		for (it = freqMap.begin(); it == freqMap.end(); it++){
 			std::pair <std::vector<symbol_t>, int> symbolList = {{it->first},it->second};
 			symbolSort.push(symbolList);
 			// We then take every element of freqMap and push it into symbolSort, after adjusting its structure to make the pair <vector of symbols, int>
@@ -50,19 +55,21 @@ namespace huffman {
 
 		while (symbolSort.size() != 1){ // We want to stop when the queue has only one element in it,
 										// since we push each composite element back into the queue
-			auto leftNode = symbolSort.top(); //reminder: firstNode is of the format pair{vector{},int}
-			symbolSort.pop;
+			auto leftNode = symbolSort.top(); //reminder: left & right Nodes are of the format pair{vector{},int}
+			symbolSort.pop();
 			auto rightNode = symbolSort.top(); 
-			symbolSort.pop;
-			for (int i = 0; i < firstNode.first.size(); ++i){
-				symbol_t current = firstNode.first[i];
-				encMap[current].insert(MyVector.begin(), 0) // Instead of using a tree, I just prepend a 0 or 1 to the encoding of the symbol in that node and all children nodes
+			symbolSort.pop();
+			for (unsigned long i = 0; i < leftNode.first.size(); ++i){
+				symbol_t current = leftNode.first[i];
+				encMap[current].insert(encMap[current].begin(), ZERO); // Instead of using a tree, I just prepend a 0 or 1 to the encoding of the symbol in that node and all children nodes
 			}												// to represent the path that would have been taken in the actual tree. I have to prepend the number here, since I'm building the path backwards
-			for (int i = 0; i < secondNode.first.size(); ++i){
-				current = secondNode.first[i];
-				encMap[current].insert(MyVector.begin(), 1);
+			for (unsigned long i = 0; i < rightNode.first.size(); ++i){
+				symbol_t current = rightNode.first[i];
+				encMap[current].insert(encMap[current].begin(), ONE);
 			}
-			auto newNode = {leftNode.first.insert(leftNode.first.end(), rightNode.first.begin(), rightNode.first.end()),(leftNode.second+rightNode.second)};
+			std::vector<symbol_t> childList = leftNode.first;
+			childList.insert(childList.end(), rightNode.first.begin(), rightNode.first.end());
+			std::pair<std::vector<symbol_t>, int> newNode = {childList,leftNode.second+rightNode.second};
 			// I'm using the vector inside the pair to keep track of which nodes are the children of this one. With regards to encoding, it doesn't matter
 			// where they are on the tree. In this way, we build up the path it would have taken without having to build the actual tree itself. 
 			symbolSort.push(newNode);
@@ -70,12 +77,12 @@ namespace huffman {
 	};
 
 ///////////////////////////////////////////////////////////////////////////////
-	Huffman::Huffman()
+	Huffman::Huffman() noexcept
 	:pImpl_(new Impl)
 	{}
 
 ///////////////////////////////////////////////////////////////////////////////
-	Huffman::~Huffman()
+	Huffman::~Huffman() noexcept
 	{}
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -83,28 +90,58 @@ namespace huffman {
 	Huffman::incFreq(symbol_t symbol)
 	{
 		pImpl_->addSymbol(symbol);
+		pImpl_->edited = true;
 	}
 
 
 ///////////////////////////////////////////////////////////////////////////////
-	Huffman::encoding_t
-	Huffman::encode(symbol_t symbol)
+	encoding_t
+	Huffman::encode(symbol_t symbol) const
 	{
-		pImpl_->makeTree();
+		if (pImpl_->edited == true){
+			pImpl_->makeTree();
+			pImpl_->edited = false;
+		};
 		return pImpl_->encMap[symbol];
 	}
 
 ///////////////////////////////////////////////////////////////////////////////
-	Huffman::decode(enc_iter_t& begin, const enc_iter_t& end)
+	symbol_t
+	Huffman::decode(enc_iter_t& begin, const enc_iter_t& end) const
 	{
-		pImpl_->makeTree();
-
+		if (pImpl_->edited == true){
+			pImpl_->makeTree();
+			pImpl_->edited = false;
+		};
+		int output = 300;
+		int bitCount = 1;
+		while (output == 300){
+			if (begin+bitCount > end){
+				throw std::runtime_error("Symbol not found");
+			}
+			encoding_t input(begin, begin+bitCount); 
+			std::unordered_map<symbol_t, encoding_t>::iterator it; 
+			for (it = pImpl_->encMap.begin(); it == pImpl_->encMap.end(); it++){
+				if (it->second == input){ // it's not possible for a leaf to continue leading on to another branch, so
+					output = it->first; // if the subvector is also an encoding in encMap, then we've found its symbol decoding. 
+					break;
+				};
+			bitCount++;
+			};
+		};
+		begin += bitCount;
+		symbol_t eightBitOutput = output;
+		return eightBitOutput;
 	}
 
 ///////////////////////////////////////////////////////////////////////////////
-	Huffman::eofCode()
-	{};
-} //namespace finish
+	encoding_t
+	Huffman::eofCode() const
+	{
+		return (Huffman::encode(end));
+	};
+} 
+//namespace finish
 
 
 
